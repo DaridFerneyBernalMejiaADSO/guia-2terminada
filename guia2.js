7876d03540695babcb4940862532de31764625cb
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-21 18:34:38 -0500

    ejercicio 5

--- guia2.js
+++ guia2.js
@@ -1,22 +1,8 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
-    let fig=prompt(`¿a cual figura le quiere calcular el area? triangulo(T), circulo(C)`)
-    if (fig==`T`){
-        let base=Number(prompt(`ingrese la base`))
-        let altura=Number(prompt(`ingrese la altura`))
-        let resul=(base*altura)/2
-        console.log("el area del triangulo es ",resul)
-    }
-    if (fig==`C`){
-        let rad=Number(prompt(`ingrese el valor del radio`))
-        let resul=Math.PI*rad**2
-        console.log("el area del circulo es",resul)
-
-    }
-    else{
-        console.log(`lo ingresado no es valido`)
-    }
+    //area del rectangulo
+    let lad=Number(prompt(`ingrese el valor del rectangulo`))
+    let area=lad*lad
+    console.log(`area del rectangulo es ${area}`)
 
 
- 
 })
