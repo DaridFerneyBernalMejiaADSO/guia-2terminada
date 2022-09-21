68b9410bdf891e8c55dc1f464f8319a718aa4759
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-21 18:13:02 -0500

    ejercicio 3

--- guia2.js
+++ guia2.js
@@ -1,18 +1,31 @@
 addEventListener("DOMContentLoaded", ()=>{
-// # Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
-let num1= Number(prompt(`ingrese su numero 1`))
-let num2= Number(prompt(`ingrese su numero 2`))
-let num3= Number(prompt(`ingrese su numero 3`))
-if(num1>num2 && num1>num3){
-    console.log(`numero 1 es mayor`)
+// # Desarrollar un diagrama que lea 3 valores diferentes
+let valor1= Number(prompt("ingrese el valor 1="))
+let valor2= Number(prompt("ingrese el valor 2="))
+let valor3= Number(prompt("ingrese el valor 3="))
+if( valor1<valor2 && valor1<valor3){
+    console.log(`el valor 1 es menor`,valor1)
 }
-if (num2>num3 && num2>num1){
-    console.log(`numero 2 es mayor`)
+else if (valor2<valor3 && valor2<valor1){
+    console.log(`el valor 2 es menor`,valor2)
 }
-else{
-    console.log(`el numero 3 es mayor`)
+else if ( valor3<valor2 && valor3<valor1){
+    console.log(`el valor 3 es menor `,valor3)
 }
+if (valor3<valor2 && valor3<valor1){
+    console.log(`el valor 1 es mayor`,valor1)
+}
+else if(valor2>valor3 && valor2>valor1){
+    console.log(`valor 2 es mayor`,valor2)
+}
+else if (valor3>valor2 && valor3>valor1){
+    console.log(`el valor 3 es mayor`,valor3)
+}
+if(valor3==valor2 && valor3 == valor1){
+    console.log(`el valor 1 igual 2`,valor1,valor2)
+}
+else if(valor2 == valor1){
+    console.log(`el valor 2 es igual al valor 1`,valor2,valor3)
 
-
+}
 })
- 
