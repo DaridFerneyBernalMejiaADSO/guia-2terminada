93f6f46846860a8771f0a370c6eb1572cdf5ae9c
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-23 11:34:38 -0500

    ejercicio 9

--- guia2.js
+++ guia2.js
@@ -1,24 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta,
-    // la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga
-    // a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.
- let tra=prompt(`ingrese en que trabaja planta (P) o administrador (A)`)
- .toUpperCase()
- if (tra==`P`){
-    let h=Number(prompt(`cantidad de horas trabajas al dia?`))
-    let sh=20000
-    let suel=h*sh
-    console.log(`pago de trabajadores de planta segun las horas`,suel)
- }
- else if (tra==`A`){
-    let h=Number(prompt(`cantidad de horas trabajas al dia?`))
-    let sho=10000
-    let sue= h*sho
-    console.log(`pago administradores administradores segun la horas`,sue)
-    
- }
-else {
-    console.log(`invalido`)
-}
+    // #realice el algoritmo de numero par o impar
+    let num=Number(prompt(`coloque un  numero`))
+    let num2=Number(prompt(`coloque otro  numero`))
+    if (num%2 == 0 && num2%2==0){
+        console.log(`el numero es par`)
+    }
+    else{
+        console.log(`el numero es impar`)
+    }
+
 })
 
