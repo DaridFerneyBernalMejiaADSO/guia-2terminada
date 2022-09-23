dc3909f937010c26454022edf7f725220f2b3431
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-23 08:01:39 -0500

    ejercicio 6

--- guia2.js
+++ guia2.js
@@ -1,8 +1,24 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //area del rectangulo
-    let lad=Number(prompt(`ingrese el valor del rectangulo`))
-    let area=lad*lad
-    console.log(`area del rectangulo es ${area}`)
+    // Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre
+    // en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es
+    // mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal
+//temperatura y presion 
+let temperatura=Number(prompt("ingrese la temperatura"))
+if (temperatura > 100){
+    console.log(`temperatura: alarma`)
+}
+else{
+    console.log(`temperatura: normal`)
+} 
+let presion=Number(prompt(`ingrese la presion`))
+if (presion>200){
+    console.log(`presion: alarma`)
+}
+else {
+    console.log(`presion: normal`)
+}
 
 
+
+    
 })
