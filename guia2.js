4dc8a16c08a5e3d616b0133af06532440bb7f69b
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-24 10:16:28 -0500

    ejercicio 11

--- guia2.js
+++ guia2.js
@@ -1,14 +1,35 @@
 addEventListener("DOMContentLoaded", ()=>{
- let prod1=Number(prompt(`ingrese el valor del producto 1 `))
- let prod2=Number(prompt(`ingrese el valor del producto 2 `))
- let prod3=Number(prompt(`ingrese el valor del producto 3 `))
- let prod4=Number(prompt(`ingrese el valor del producto 4 `))
- let prod5=Number(prompt(`ingrese el valor del producto 5 `))
- let IVA=0.19
- let subtot= prod1+prod2+prod3+prod4+prod5
- let iva= subtot*IVA
- let total= subtot+iva
- console.log(`eel subtotal es`,subtot)
- console.log(`el IVA es igual:`,iva)
- console.log(`el total es:`,total)
+//programa nombre, apellido edad
+let nombre=prompt(`ingrese su nombre`)
+let apellido=prompt(`ingrese su apellido`)
+let edad= Number(prompt(`ingrese su edad`))
+let nota1=Number(prompt(`ingrese la nota 1 :`))
+let nota2=Number(prompt(`ingrese la nota 2 :`))
+let nota3=Number(prompt(`ingrese la nota 3 :`))
+let promed=(nota1+nota2+nota3)/3
+if(promed > 59)[
+    console.log(`el estudiante ${nombre} ${apellido} su edad ${edad} fue aprobado `)
+]
+else[
+    console.log(`el estudiante ${nombre} ${apellido} su edad ${edad} No fue aprobado `)
+]
+console.log(`el promedio del grado  es `,promed)
+
+
+
+
 })
+//     #programa de nombre, apellido edad,
+//     nombre=(input("ingrese sus nombres"))
+//     apellido=(input("ingreses sus apellidos"))
+//     edad=int(input("ingrese su edad"))
+    
+//     nota1=int(input("ingrese la primera nota"))
+//     nota2=int(input("ingrese la segunda nota"))
+//     nota3=int(input("ingrese la tercera nota"))
+//     promed=(nota1+nota2+nota3)/3
+//     if promed > 59:
+//       print("aprobado")
+//     else:
+//       print("no aprobado")
+//     print("el promedio es",promed)    
