3b5667f04424e17371fa931004af02a32bf777dc
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-24 09:59:37 -0500

    ejercicio 10

--- guia2.js
+++ guia2.js
@@ -1,13 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // #realice el algoritmo de numero par o impar
-    let num=Number(prompt(`coloque un  numero`))
-    let num2=Number(prompt(`coloque otro  numero`))
-    if (num%2 == 0 && num2%2==0){
-        console.log(`el numero es par`)
-    }
-    else{
-        console.log(`el numero es impar`)
-    }
-
+ let prod1=Number(prompt(`ingrese el valor del producto 1 `))
+ let prod2=Number(prompt(`ingrese el valor del producto 2 `))
+ let prod3=Number(prompt(`ingrese el valor del producto 3 `))
+ let prod4=Number(prompt(`ingrese el valor del producto 4 `))
+ let prod5=Number(prompt(`ingrese el valor del producto 5 `))
+ let IVA=0.19
+ let subtot= prod1+prod2+prod3+prod4+prod5
+ let iva= subtot*IVA
+ let total= subtot+iva
+ console.log(`eel subtotal es`,subtot)
+ console.log(`el IVA es igual:`,iva)
+ console.log(`el total es:`,total)
 })
-
