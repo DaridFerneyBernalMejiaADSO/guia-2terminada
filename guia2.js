b3fd02d11a165dea10e62181f1e9a247856ee7ae
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-24 10:38:05 -0500

    ejercicio 13

--- guia2.js
+++ guia2.js
@@ -1,7 +1,19 @@
 addEventListener("DOMContentLoaded", ()=>{
-// #la resta de dos numeros
-let num1=Number(prompt("ingrese un numero"))
-let num2=Number(prompt("ingrese un segundo numero"))
-console.log(`la resta es `,num1-num2)
-    
+// en una tienda de helado
+let prod=Number(prompt(`ingrese el precio del producto`))
+let mem=prompt(`ingrese su membresia tipo (A),(B),(C)`)
+.toUpperCase()
+if (mem ==`A`){
+    let desc=prod*0.10
+    console.log(`el descuento de tipo A es:`,desc)
+
+}
+else if( mem == `B`){
+    let descb=prod*0.15
+    console.log(`el descuentom de tipo B es `,descb)
+}
+else if(mem == `C`){
+    let descc=prod*0.20
+    console.log(`el descuento tipo C es `,descc)
+}
 })
